0a24cac6c96b46ffc603ebdb8433d03fc6c67c9f
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-11-12 21:29:28 +0200

    Break all the pylint rules

--- src/varasto.py
+++ src/varasto.py
@@ -5,7 +5,7 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -16,7 +16,20 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+        if 1 < 2:
+            print("maailma on mallillaan")
+            if 2 < 3:
+                print("lait pätevät")
+                if 3 < 4:
+                    print("kaikki on hyvin")
+            print("jatketaan samaan malliin")
+        else:
+            print("jotain on vialla")
+            print("maailma on pilalla")
+            print("mennään kaikki kotiin")
+            print("piiloon peiton alle")
+
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää kuten viela_tilaa taikka muuta sellaista
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -31,7 +44,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +53,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
